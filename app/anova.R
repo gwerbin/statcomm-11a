5a4c53a3aeb3e6672ec691c37c3515b0f6d6044c
Greg Werbin <gw2286@columbia.edu>
2014-11-18 11:05:11 -0500

    text area input
    it's broken at the moment, but i think it's pretty close to working

--- app/anova.R
+++ app/anova.R
@@ -1,119 +1,110 @@
 library(colorspace)
 
-# Input ----
-n_group <- 15
-m_group <- 0
-sd_group <- 1
+plots <- new.env()
 
-n_rep <- 5000
-alpha = 0.05
+# Generate data ----
+plots$sample_unif <- function(n_sample) seq(0 + 1/n_sample, 1 - 1/n_sample, 1/n_sample)
 
-input_list <- list(
-  # list(rng = rnorm, inv_F = qnorm, n = n_group, mean = m_group + .6, sd = sd_group),
-  list(rng = rnorm, inv_F = qnorm, n = n_group, mean = m_group, sd = sd_group),
-  list(rng = rnorm, inv_F = qnorm, n = n_group, mean = m_group, sd = sd_group),
-  list(rng = rnorm, inv_F = qnorm, n = n_group, mean = m_group, sd = sd_group),
-  list(rng = rt, inv_F = qt, n = n_group, df = 2, ncp = 1)
-)
-
-K <- length(input_list)
-group_names <- paste0("G", 1:K)
-
-## Generate data ----
-
-sample_unif <- function(n_sample) seq(0 + 1/n_sample, 1 - 1/n_sample, 1/n_sample)
-
-y_pretty <- lapply(input_list, function(item) {
-  do.call(item$inv_F, c(list(sample_unif(2000)), item[-(1:3)]))
-})
-
-N <- sum(vapply(input_list, `[[`, 0, 3))
-group_sizes <- vapply(input_list, function(grp) grp$n, 0)
-group_padding <- max(group_sizes) - group_sizes
-between_df <- K - 1
-within_df <- sum(group_sizes - 1)
-
-y_rep <- vector("list", n_rep)
-group_means <- matrix(0, n_rep, K)
-grand_mean <- numeric(n_rep)
-between_ms <- between_ss <- numeric(n_rep)
-within_ms <- within_ss <- numeric(n_rep)
-f <- numeric(n_rep)
-
-progress <- txtProgressBar(max = n_rep, style = 3)
-for(r in 1:n_rep) {
-  y_r <- lapply(input_list, function(item) do.call(item[[1]], item[-(1:2)]))
-  y_rep[[r]] <- y_r
-  names(y_r) <- group_names
+plots$generate_data <- function(input_list, n_rep, alpha) {
+  d <- new.env()
   
-  grand_mean[r] <- mean(unlist(y_r))
-  
-  for(k in 1:K) {
-  	n_k <- input_list[[k]]$n
-  	group_means[r, k] <- mean(y_r[[k]])
+  d$K <- length(input_list)
+  d$group_names <- paste0("G", 1:d$K)
 
-  	between_ss[r] <- n_k * (group_means[r, k] - grand_mean[r])^2 + between_ss[r]
-  	within_ss[r] <- sum((y_r[[k]] - group_means[r, k])^2) + within_ss[r]
+  d$y_pretty <- lapply(input_list, function(item) {
+    do.call(item$inv_F, c(list(plots$sample_unif(2000)), item[-(1:3)]))
+  })
+  
+  d$N <- sum(vapply(input_list, `[[`, 0, 3))
+  d$group_sizes <- vapply(input_list, function(grp) grp$n, 0)
+  d$group_padding <- max(d$group_sizes) - d$group_sizes
+  d$between_df <- d$K - 1
+  d$within_df <- sum(d$group_sizes - 1)
+  
+  d$y_rep <<- vector("list", n_rep)
+  d$group_means <- matrix(0, n_rep, d$K)
+  d$grand_mean <- numeric(n_rep)
+  d$between_ms <- d$between_ss <- numeric(n_rep)
+  d$within_ms <- d$within_ss <- numeric(n_rep)
+  d$f <<- numeric(n_rep)
+  
+  progress <- txtProgressBar(max = n_rep, style = 3)
+  for(r in 1:n_rep) {
+    y_r <- lapply(input_list, function(item) do.call(item[[1]], item[-(1:2)]))
+    d$y_rep[[r]] <- y_r
+    names(y_r) <- d$group_names
+    
+    d$grand_mean[r] <<- mean(unlist(y_r))
+    
+    for(k in 1:K) {
+      n_k <- input_list[[k]]$n
+      d$group_means[r, k] <- mean(y_r[[k]])
+      
+      d$between_ss[r] <- n_k * (d$group_means[r, k] - d$grand_mean[r])^2 + d$between_ss[r]
+      d$within_ss[r] <- sum((y_r[[k]] - d$group_means[r, k])^2) + d$within_ss[r]
+    }
+    d$between_ms[r] <- d$between_ss[r] / d$between_df
+    d$within_ms[r] <- d$within_ss[r] / d$within_df
+    d$f[r] <- d$between_ms[r] / d$within_ms[r]
+    
+    setTxtProgressBar(progress, r)
   }
-  between_ms[r] <- between_ss[r] / between_df
-  within_ms[r] <- within_ss[r] / within_df
-  f[r] <- between_ms[r] / within_ms[r]
+  close(progress)
+
+  d$plot_colors <- rainbow_hcl(K, start = 30, end = 300)
   
-  setTxtProgressBar(progress, r)
+  invisible(d)
 }
-close(progress)
 
 # Plot data ----
-
-plot_colors <- rainbow_hcl(K, start = 30, end = 300)
  
-data_boxplot <- function() {
+plots$data_boxplot <- function(d) {
   layout(matrix(c(1,1,1,2), nrow = 1))
   ## group boxplot
   par(mar = c(3, 2, 1, 0) + .1)
-  boxplot(y_pretty, col = plot_colors, xaxt = "n", las = 1)
-  axis(1, at = 1:K, labels = group_names, tick = FALSE)
+  boxplot(d$y_pretty, col = d$plot_colors, xaxt = "n", las = 1)
+  axis(1, at = 1:K, labels = d$group_names, tick = FALSE)
   
   ## overall boxplot
   par(mar = c(3, 0, 1, 1) + .1)
-  boxplot(unlist(y_pretty), xaxt = "n", yaxt = "n")
+  boxplot(unlist(d$y_pretty), xaxt = "n", yaxt = "n")
   axis(1, at = 1, labels = "Overall", tick = FALSE)
 }
 
-sampling_distribution_plot <- function(){
+plots$sampling_distribution_plot <- function(d){
   ## sampling distributions
   # par(mar = c(3, 1, 2.5, 1) + .1)
-  plot(density(grand_mean), lwd = 2, lty = "dashed", col = "darkgray",
+  plot(density(d$grand_mean), lwd = 2, lty = "dashed", col = "darkgray",
        xlab = "", ylab = "", main = "", yaxt = "n",
-       xlim = c(min(group_means), max(group_means)))
-  for(k in 1:K) lines(density(group_means[, k]), lwd = 2, col = plot_colors[k])
+       xlim = c(min(d$group_means), max(d$group_means)))
+  for(k in 1:K) lines(density(d$group_means[, k]), lwd = 2, col = d$plot_colors[k])
 }
 
-mean_squares_plot <- function() {
-  between_den <- density(between_ms)
-  within_den <- density(within_ms)
+plots$mean_squares_plot <- function(d) {
+  between_den <- density(d$between_ms)
+  within_den <- density(d$within_ms)
   ## between MS
   # par(mar = c(1, 0, 2, 1) + .1)
-  plot(between_den, lwd = 2, lty = "dashed",
+  plot(d$between_den, lwd = 2, lty = "dashed",
        main = "", xlab = "", ylab = "", yaxt = "n",
-       ylim = c(0, max(within_den$y)))
+       ylim = c(0, max(d$within_den$y)))
   
   ## within MS
   # par(mar = c(1, 0, 1, 1) + .1)
-  lines(within_den, lwd = 2, lty = "dashed",
+  lines(d$within_den, lwd = 2, lty = "dashed",
        main = "", xlab = "", ylab = "", yaxt = "n")
 }
 
-f_stat_plot <- function() {
-  h <- hist(f, breaks = 50, plot = FALSE)
+plots$f_stat_plot <- function(d) {
+  h <- hist(d$f, breaks = 50, plot = FALSE)
   xlim <- c(min(h$breaks), max(h$breaks))
   x <- seq(xlim[1], xlim[2], 0.1)
-  x_den <- df(x, between_df, within_df)
+  x_den <- df(x, d$between_df, d$within_df)
   ylim <- c(0, max(max(x_den), max(h$density)) + 0.1)
   plot(h, freq = FALSE, ylim = ylim, main = "", xlab = "", ylab = "")
   lines(x, x_den, xlim = xlim)
   
-  f_alpha <- qf(1 - alpha, between_df, within_df)
+  f_alpha <- qf(1 - d$alpha, d$between_df, d$within_df)
   rect(0, xlim[1], f_alpha, ylim[2],
        border = NA,
        col = do.call("rgb", as.list(c(col2rgb("steelblue")/255, 1/4))))
@@ -122,8 +113,8 @@ f_stat_plot <- function() {
        col = do.call("rgb", as.list(c(col2rgb("wheat")/255, 1/4))))
   
   legend("topright",
-         c(sprintf("theoretical coverage: %0.0f%%", pf(f_alpha, between_df, within_df) * 100),
-           sprintf("empirical coverage: %0.0f%%", ecdf(f)(f_alpha) * 100)),
+         c(sprintf("theoretical coverage: %0.0f%%", pf(f_alpha, d$between_df, d$within_df) * 100),
+           sprintf("empirical coverage: %0.0f%%", ecdf(d$f)(f_alpha) * 100)),
          bty = "n", inset = 5/100)
   mtext(
     side = 3,
--- app/server.R
+++ app/server.R
@@ -1,20 +1,21 @@
 library(shiny)
 
-source("./anova.R")
-
 shinyServer(function (input, output, session) { 
   
-#   observe({
-#     
-#   })
-#   
-#   reactive({
-#     
-#   })
+  input_list <- reactive(eval(parse(text = input$input_list)))
+  n_rep <- reactive(input$n_rep)
+  alpha <- reactive(input$alpha)
   
-  output$boxplots <- renderPlot(data_boxplot())
-  output$sampling_dist <- renderPlot(sampling_distribution_plot())
-  output$mean_squares <- renderPlot(mean_squares_plot())
-  output$f_stat <- renderPlot(f_stat_plot())
+  plot_env <- reactive({
+    source("anova.R")
+    plots$d <- plots$generate_data(input_list, n_rep, alpha)
+    plots
+  })
+  
+  output$boxplots <- renderPlot(plot_env()$data_boxplot(plot_env()$d))
+  output$sampling_dist <- renderPlot(plot_env()$sampling_distribution_plot(plot_env()$d))
+  output$mean_squares <- renderPlot(plot_env()$mean_squares_plot(plot_env()$d))
+#     {plot.new(); text(0, 0, length(y_rep))})
+  output$f_stat <- renderPlot(plot_env()$f_stat_plot(plot_env()$d))
   
 })
